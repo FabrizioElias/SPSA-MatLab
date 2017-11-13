98c714e83faafcec8f4eb3eb1fea3d322574cade
Fabrizio Elias <fabrizioelias@protonmail.com>
2017-11-13 18:39:01 -0300

    Remoção de arquivos sem utilidade e adição de arquivo contendo as dependências de código.

--- ROTINAS_new (MEXIDO SEM QUERER)/amostrasolo.m
+++
@@ -1,6 +0,0 @@
-function [CARGASOLO]=amostrasolo(DADOS, arquivoinput, PORTICO)
-
-[PORTICO]=Le_DadosGAMBsolo(DADOS, arquivoinput, PORTICO);
-
-[CARGASOLO]=loadsGAMB(DADOS, PORTICO);
-
--- ROTINAS_new (MEXIDO SEM QUERER)/plotdiag.m
+++
@@ -1,9 +0,0 @@
-function [PILAR]=plotdiag(PILAR, COMBColumn)
-%P=PILAR.Pn;
-P=PILAR.NIn;
-%M=PILAR.Mn;
-M=PILAR.MIn;
-%plot(M,P,'-ro','LineWidth',5','MarkerSize',10)
-plot(M,P,'-ro','LineWidth',5')
-%plot(M,P)
-hold on
